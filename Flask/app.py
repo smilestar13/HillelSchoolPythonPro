be4debad9511cd3a4b95d3f9bdaf13a7f36704d1
smilestar13 <oleksienko.boris@gmail.com>
2023-03-12 15:30:50 +0200

    FLASK. HW-3 is DONE. Version 1.0

--- Flask/app.py
+++ Flask/app.py
@@ -1,18 +1,49 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return 'Hello user!'
+    return 'Hello :)'
 
 
 @app.route("/registration", methods=['GET', 'POST'])
 def registration(*args, **kwargs):
     context = {}
+
     if request.method == 'POST':
         name = request.form.get('name')
         password = request.form.get('password')
-        context.update({'name': name, 'password': password})
-        return render_template('index.html', **context)
+
+        with open('user_data.log', 'r') as work_one:
+            for el in work_one:
+                separator = el.find('- PASS -')
+                if f" '{name}' " in el[:separator]:
+                    return redirect(url_for('login'))
+
+        with open('user_data.log', 'a') as work_one:
+            work_one.write(f"- NAME - > '{name}' - PASS - > '{password}'\n")
+            context.update({'result': f'Dear, {name}: congratulations on your registration!'})
+
+    return render_template('registration.html', **context)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login(*args, **kwargs):
+    context = {}
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        password = request.form.get('password')
+
+        with open('user_data.log', 'r') as work_one:
+            for el in work_one:
+                separator = el.find('- PASS -')
+                if f" '{name}' " in el[:separator] and f" '{password}'" in el[separator:]:
+                    context.update({'result': f'Dear, {name}: You are in a system!'})
+                    return render_template('login.html', **context)
+                else:
+                    context.update({'result': f'Invalid user data!!! Try again...'})
+
+    return render_template('login.html', **context)
