b289b429c0c57109984ba243b69b2983de526c93
smilestar13 <oleksienko.boris@gmail.com>
2023-03-30 19:51:35 +0300

    Django lesson #6 - HW.

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env(BASE_DIR / '.env')
 SECRET_KEY = env.str('SECRET_KEY', default='SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=True )
+DEBUG = env.bool('DEBUG', default=True)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
