c725baa18b08e86ecf23ef1c584d667beaf9e92c
smilestar13 <oleksienko.boris@gmail.com>
2023-03-30 19:31:19 +0300

    Django lesson #6 - HW.

--- products/models.py
+++ products/models.py
@@ -1,21 +1,39 @@
-# from os import path
-import uuid
+from os import path
 
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# def upload_to(instance, filename):
-#     _name, extension = path.splitext(filename)
-#     return f'products/images/{str(instance.pk)}{extension}'
+from project.constants import MAX_DIGITS, DECIMAL_PLACES
+from project.mixins.models import PKMixin
 
-class Product(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+def upload_to(instance, filename):
+    _name, extension = path.splitext(filename)
+    return f'products/images/{str(instance.pk)}{extension}'
+
+
+class Category(PKMixin):
+    name = models.CharField(max_length=128)
+    description = models.TextField(
+        blank=True,
+        null=True
+    )
+    image = models.ImageField(upload_to=upload_to, null=True, blank=True)
+
+
+class Product(PKMixin):
     name = models.CharField(max_length=255)
     description = models.TextField(
         blank=True,
         null=True
     )
-    # image = models.ImageField(upload_to=upload_to)
+    image = models.ImageField(upload_to=upload_to, null=True, blank=True)
     sku = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    price = models.DecimalField(
+        validators=[MinValueValidator(0)],
+        max_digits=MAX_DIGITS,
+        decimal_places=DECIMAL_PLACES)
+    categories = models.ManyToManyField(Category, blank=True)
+    products = models.ManyToManyField("products.Product", blank=True)
+
