5b396310fa2f76a18b1cf9153410e6ed3fb45909
smilestar13 <oleksienko.boris@gmail.com>
2023-04-03 20:18:30 +0300

    Django. Lesson #7 - HW.

--- products/admin.py
+++ products/admin.py
@@ -6,12 +6,12 @@ from project.mixins.admins import ImageSnapshotAdminMixin
 
 @admin.register(Product)
 class ProductAdmin(ImageSnapshotAdminMixin, admin.ModelAdmin):
-    list_display = ('name', 'price', 'is_active', 'image_tag')
+    list_display = ('name', 'price', 'is_active')
     filter_horizontal = ('categories', 'products')
     readonly_fields = ('image_tag', )
 
 
 @admin.register(Category)
 class CategoryAdmin(ImageSnapshotAdminMixin, admin.ModelAdmin):
-    list_display = ('name', 'description', 'image_tag')
+    list_display = ('name', 'description')
     readonly_fields = ('image_tag', )
--- project/urls.py
+++ project/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from products.urls import urlpatterns as products_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include(products_urlpatterns))
+    path('products/', include(products_urlpatterns)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
