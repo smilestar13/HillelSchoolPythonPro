bd2d95f9b8b378e391b8a4c491950fce4f93cf4a
smilestar13 <oleksienko.boris@gmail.com>
2023-04-24 20:20:01 +0300

    Lesson 11. HW csv and pdf

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from products.views import ProductsView
+from products.views import ProductsView, ExportToPdf, ExportToCSV
 
 from django.conf import settings
 
 
 urlpatterns = [
-    path('', ProductsView.as_view(), name='products')
+    path('', ProductsView.as_view(), name='products'),
+    path('export-csv/', ExportToCSV.as_view(), name='products_to_csv'),
+    path('export-pdf/', ExportToPdf.as_view(), name='products_to_pdf')
 ]
 
 if settings.DEBUG:
--- products/views.py
+++ products/views.py
@@ -1,4 +1,12 @@
+from django.template.loader import render_to_string
+from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
+from django.views import View
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
+import csv
+import weasyprint
 
 from products.forms import ProductModelForm
 from products.models import Product
@@ -17,3 +25,48 @@ class ProductsView(FormView):
         context = super().get_context_data(**kwargs)
         context['products'] = Product.objects.iterator()
         return context
+
+
+@login_required
+class ExportToCSV(View):
+
+    def get(self, request, *args, **kwargs):
+        headers = {
+            'Content-Type': 'text/csv',
+            'Content-Disposition': 'attachment; filename="products.csv"'
+        }
+        response = HttpResponse(headers=headers)
+        fields_name = ['name', 'description', 'sku', 'image', 'price', 'is_active']
+        writer = csv.DictWriter(response, fieldnames=fields_name)
+        writer.writeheader()
+        for product in Product.objects.iterator():
+            writer.writerow(
+                {
+                    'name': product.name,
+                    'description': product.description,
+                    'image': product.image.name if product.image else 'no image',
+                    'sku': product.sku,
+                    'price': product.price,
+                    'is_active': product.is_active
+                }
+            )
+        return response
+
+
+@login_required
+class ExportToPdf(TemplateView):
+    template_name = 'products/pdf.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {'products': Product.objects.all()}
+        headers = {
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'attachment; filename="products.pdf"'
+        }
+        html = render_to_string(
+            template_name=self.template_name,
+            context=context
+        )
+        pdf = weasyprint.HTML(string=html).write_pdf()
+        response = HttpResponse(pdf, headers=headers)
+        return response
