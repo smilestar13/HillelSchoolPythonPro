c5c18e3351c670f424460d29a1cb6dba2bb1131c
smilestar13 <oleksienko.boris@gmail.com>
2023-05-15 22:31:32 +0300

    Lesson 17. HW Messages framework, auth backend

--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,9 @@ from django.utils.translation import gettext_lazy as _
 from django.views.generic import FormView
 from django.shortcuts import render, redirect
 from django.views import View
+import random
+import string
+import time
 
 from accounts.model_forms import RegistrationForm, AuthenticationForm
 
@@ -45,6 +48,9 @@ class ProfileView(View):
         return render(request, self.template_name, {'user': user})
 
 
+import time
+from django.core.cache import cache
+
 class UpdatePhoneView(View):
     template_name = 'registration/update_phone.html'
 
@@ -53,10 +59,35 @@ class UpdatePhoneView(View):
         return super().dispatch(request, *args, **kwargs)
 
     def get(self, request):
-        return render(request, self.template_name)
+        confirmation_code = cache.get('confirmation_code')
+        current_time = int(time.time())
+
+        if not confirmation_code or (current_time - cache.get('code_generated_time', 0)) >= 60:
+            confirmation_code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+            cache.set('confirmation_code', confirmation_code, 60)
+            cache.set('code_generated_time', current_time, 60)
+
+        context = {
+            'confirmation_code': confirmation_code
+        }
+        return render(request, self.template_name, context)
 
     def post(self, request):
         user = request.user
-        user.phone = request.POST.get('phone_number')
-        user.save()
-        return redirect('profile')
+        phone = request.POST.get('phone')
+        code = request.POST.get('code')
+        confirmation_code = cache.get('confirmation_code')
+
+        if code == confirmation_code:
+            user.phone = phone
+            user.save()
+            return redirect('profile')
+        else:
+            cache.delete('confirmation_code')
+            cache.delete('code_generated_time')
+            context = {
+                'confirmation_code': confirmation_code,
+                'error_message': 'Invalid confirmation code. Please try again.'
+            }
+            return render(request, self.template_name, context)
+
